f43db1fb7d759ccba1b0fb27b461385f5bdaf7b9
John C Nash <nashjc@uottawa.ca>
2012-03-31 00:00:00 +0000

    version 2012-3.31

--- R/optimx.R
+++ R/optimx.R
@@ -675,7 +675,7 @@ scalecheck<-function(par, lower=lower, upper=upper,dowarn){
 		if (ctrl$trace>0) cat("Rvmmin failed for current problem \n")
 		ans<-list(fevals=NA) # ans not yet defined, so set as list
 ##		ans$value<-ans$fvalue 
-##		ans$value= ctrl$badval
+		ans$value= ctrl$badval
 		ans$par<-rep(NA,npar)
                 ans$conv<-9999 # failed in run
         	ans$gevals<-NA 
